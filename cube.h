e74dc4352b1328064944f4f9b2ca82510c5410e4
Jan Breja <johnbrejawork@gmail.com>
2026-07-06 19:22:57 +0200

    First implementation of .h file

--- cube.h
+++ cube.h
@@ -1,5 +1,5 @@
 
-
+// Structure of Rubic's Cube (later RB)
 typedef enum {
     WHITE,
     BLUE,
@@ -10,18 +10,57 @@ typedef enum {
 } Color;
 
 typedef enum {
-    FRONT,
-    BACK,
-    RIGHT,
-    LEFT,
-    UP,
-    DOWN
+    FRONT = 0,
+    BACK = 1,
+    RIGHT = 2,
+    LEFT = 3,
+    UP = 4,
+    DOWN = 5
 } FaceIndex;
+
+typedef enum { LEFT_TO_RIGHT, RIGHT_TO_LEFT } RowDir;
+typedef enum { TOP_TO_BOTTOM, BOTTOM_TO_TOP } ColDir;
 
 typedef struct {
     Color cells[3][3];
 } Face;
 
 typedef struct {
-    Face sides[FRONT, BACK, RIGHT, LEFT, UP, DOWN];
+    Face sides[6];
 } Cube;
+
+
+// ----- Main function for RB -----
+
+Cube *init_cube();
+void free_cube(Cube *cube);
+void print_cube(Cube *cube);
+
+void solve_cube(Cube *cube);
+void randomise_cube(Cube *cube);
+
+bool is_solved(Cube *cube);
+int count_moves(Cube *cube);
+
+// ----- Solving functions -----
+
+// Solving is split into two layers:
+// Low-level (rotate_face_grid, shift_row, shift_col) handle raw grid manipulation
+
+// High-level moves (move_U, move_R, ...) call low-level functions to represent
+// actual cube moves.
+
+// Low level
+
+void shift_row(Cube *cube, int row, RowDir dir);
+void shift_col(Cube *cube, int col, ColDir dir);
+void rotate_face(Cube *cube, FaceIndex face, bool clockwise);
+
+// High level
+
+void move_UP(Cube *cube, bool clockwise); 
+void move_DOWN(Cube *cube, bool clockwise); 
+void move_LEFT(Cube *cube, bool clockwise); 
+void move_RIGHT(Cube *cube, bool clockwise); 
+void move_FRONT(Cube *cube, bool clockwise); 
+void move_BACK(Cube *cube, bool clockwise); 
